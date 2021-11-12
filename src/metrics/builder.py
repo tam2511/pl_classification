9758f77e65ca3eb106a9dfd6a999bd735f263f12
data_sanchez <tam2511@mail.ru>
2021-11-12 19:43:28 +0300

    to modeldict

--- src/metrics/builder.py
+++ src/metrics/builder.py
@@ -1,5 +1,5 @@
 from torchmetrics import Metric
-from torch.nn import ModuleList
+from torch.nn import ModuleDict
 import torch
 from metrics.metric import TorchMetric
 from metrics.best_threshold_stats import BestThresholdStats
@@ -25,29 +25,29 @@ def create_metric__(metric_info, class_names=None):
 class MetricsList(Metric):
     def __init__(self, metrics_infos, class_names=None, dist_sync_on_step=False, compute_on_step=True):
         super().__init__(dist_sync_on_step=dist_sync_on_step, compute_on_step=compute_on_step)
-        self.metrics = ModuleList()
+        self.metrics = ModuleDict()
         for idx, metric_info in enumerate(metrics_infos):
             metric = create_metric__(metric_info, class_names)
-            self.metrics.append(module=metric)
+            self.metrics.add_module(name=f'{idx}', module=metric)
 
     def update(self, preds: torch.Tensor, target: torch.Tensor):
         assert preds.shape == target.shape
         for metric_idx in range(len(self.metrics)):
             # TODO: optimize metrics device passing
-            self.metrics[metric_idx].to(preds.device)
-            self.metrics[metric_idx].update(preds, target)
+            self.metrics[f'{metric_idx}'].to(preds.device)
+            self.metrics[f'{metric_idx}'].update(preds, target)
 
     def compute(self):
         result = []
         for metric_idx in range(len(self.metrics)):
-            result_ = self.metrics[metric_idx].compute()
+            result_ = self.metrics[f'{metric_idx}'].compute()
             for name in result_:
                 result[name] = result_[name]
         return result
 
     def reset(self):
         for metric_idx in range(len(self.metrics)):
-            self.metrics[metric_idx].reset()
+            self.metrics[f'{metric_idx}'].reset()
 
     def add(self, metric: Metric):
-        self.metrics.append(module=metric)
+        self.metrics.append(name=f'{len(self.metrics)}', module=metric)
