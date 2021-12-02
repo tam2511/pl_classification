bb16207ce40be2647d12c1e5521e21905cbcf693
data_sanchez <tam2511@mail.ru>
2021-12-03 01:34:54 +0300

    add triplets_learner

--- src/datasets/triplets_csv_dataset.py
+++ src/datasets/triplets_csv_dataset.py
@@ -12,7 +12,12 @@ class TripletsCSVDataset(Dataset):
     Csv dataset representation (csv will be in RAM) for triplets
     '''
 
-    def __init__(self, csv_path: str, image_prefix='', path_transform=None, transform=None, return_triplets=True):
+    def __init__(self,
+                 csv_path: str,
+                 image_prefix='',
+                 path_transform=None,
+                 transform=None,
+                 return_triplets=True):
         '''
         :param csv_path: path to csv file with paths of images (one column)
         :param image_prefix: path prefix which will be added to paths of images in csv file
@@ -28,7 +33,7 @@ class TripletsCSVDataset(Dataset):
         self.dt = pd.read_csv(csv_path)
         self.return_triplets = return_triplets
         images_per_classes = self.dt.iloc[:, 1].apply(lambda x: len(x.split(' '))).values
-        self.idxs = np.zeros((images_per_classes.sum(), 2))
+        self.idxs = np.zeros((images_per_classes.sum(), 2), dtype=np.int64)
         it = 0
         for i in range(len(images_per_classes)):
             self.idxs[it: it + images_per_classes[i], 0] = i
@@ -70,10 +75,10 @@ class TripletsCSVDataset(Dataset):
         anchor_id = positive_image_ids[image_idx]
         anchor_image = self.__read_image(anchor_id)
         if not self.return_triplets:
-            return anchor_image, label_idx
+            return (anchor_image,), label_idx
         positive_id = self.__get_positive_id(positive_image_ids, image_idx)
         positive_image = self.__read_image(positive_id)
 
         negative_id = self.__get_negative_id(label_idx)
         negative_image = self.__read_image(negative_id)
-        return anchor_image, positive_image, negative_image
+        return (anchor_image, positive_image, negative_image), row[0]
