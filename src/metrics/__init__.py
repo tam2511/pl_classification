9a6363d1fba2d1d6fcf1ac8b3a49fb45700f407f
data_sanchez <tam2511@mail.ru>
2021-12-09 01:18:32 +0300

    imports fix

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -1,3 +1,4 @@
-from metrics.torchmetrics import TorchMetric
+from metrics.wrapper import TorchMetric
 from metrics.best_threshold_stats import BestThresholdStats
 from metrics.search_accuracy import SearchAccuracy
+from metrics.metric import MetricsList
