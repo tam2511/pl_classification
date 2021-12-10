04cb7e6366e1e401f456c543ba7c0bf666735711
data_sanchez <tam2511@mail.ru>
2021-12-10 23:47:58 +0300

    fix classificator_learner

--- src/learners/classificator_learner.py
+++ src/learners/classificator_learner.py
@@ -11,6 +11,7 @@ class ClassificatorLearner(BaseLearner):
             model: torch.nn.Module,
             loss: torch.nn.Module,
             optimizer: Optimizer,
+            multilabel: bool = False,
             lr_scheduler: Scheduler = None,
             train_metrics: list = None,
             val_metrics: list = None,
@@ -19,14 +20,15 @@ class ClassificatorLearner(BaseLearner):
     ):
         super().__init__(model, loss, optimizer, lr_scheduler, train_metrics, val_metrics, return_val_output,
                          return_train_output)
+        self.multilabel = multilabel
 
     __init__.__doc__ = BaseLearner.__init__.__doc__
 
     def common_step(self, batch, batch_idx):
         x, y = batch
         y_ = self.forward(x)
-        loss = self.loss_f(y_, y.float() if self.cfg.multilabel else y.argmax(dim=1))
-        output = y_.sigmoid() if self.cfg.multilabel else y_.argmax(dim=1)
+        loss = self.loss_f(y_, y.float() if self.multilabel else y.argmax(dim=1))
+        output = y_.sigmoid() if self.multilabel else y_.argmax(dim=1)
         target = y
-        return_output = y_.sigmoid() if self.cfg.multilabel else y_.softmax(dim=1)
+        return_output = y_.sigmoid() if self.multilabel else y_.softmax(dim=1)
         return loss, output, target, return_output
